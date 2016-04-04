d907c2689baf7c97b7085bcfdcd3e2b67a031dea
morninglili <morninglili@126.com>
2016-04-04 20:58:10 +0800

    modify models.py, admin.py

--- block/admin.py
+++ block/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from models import Demolis
+
+
+class DemoliliAdmin(admin.ModelAdmin):
+    list_display = ["example4char", "example4int", "sex", "owner", "last_update_timestamp", 'create_timestamp']
+    list_filter = ("sex",)
+
+
+admin.site.register(Demolis, DemoliliAdmin)
